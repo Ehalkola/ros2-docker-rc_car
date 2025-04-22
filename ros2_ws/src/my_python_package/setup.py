1f6be70bdac2c46b5ce4298ac430f146d211f036
Ehalkola <ehalkola@edu.lapinamk.fi>
2025-04-22 10:36:48 +0300

    zed_camera_link

--- ros2_ws/src/my_python_package/setup.py
+++ ros2_ws/src/my_python_package/setup.py
@@ -24,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "my_node = my_python_package.my_node:main"
+            "my_node = my_python_package.my_node:main",
         ],
     },
 )
